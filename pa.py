1b79f60994684afc6f8a2f043f35b9730e908409
happyhqx123 <529503542@qq.com>
2019-04-17 06:05:18 +0800

    加入正则，可以正确显示所有小说正文内容，无法显示标题，且不通用

--- pa.py
+++ pa.py
@@ -4,4 +4,6 @@ url='https://m.80txt.com/6203/31933511.html'
 html=requests.get(url)
 web=html.content.decode()
 xs=BeautifulSoup(web,'lxml')
-print(xs.get_text())
+zw=re.search(r'刘峰看着(.*?)没有留下。',xs.get_text())
+print(zw)
+
