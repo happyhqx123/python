e676b92b108ccc3b309edd1a0339b57053b87575
happyhqx123 <529503542@qq.com>
2019-04-16 21:41:10 +0800

    可以正常显示小说内容，但还有一些多余的网页代码需要正则去除

--- pa.py
+++ pa.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 url='https://m.80txt.com/6203/31933511.html'
 html=requests.get(url)
-web=html.text
+web=html.content.decode()
 xs=BeautifulSoup(web,'lxml')
 xs1=xs.br
 print(xs1)
