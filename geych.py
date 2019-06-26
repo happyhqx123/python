425a43c734e221c73ee050e32e1302b86fed403d
happyhqx123 <529503542@qq.com>
2019-06-26 09:05:00 +0000

    增加java

--- geych.py
+++ geych.py
@@ -6,12 +6,17 @@ new = termios.tcgetattr(fd)
 # turn off echo and press-enter
 new[3] = new[3] & ~termios.ECHO & ~termios.ICANON
 
+
 def getch():
     try:
         termios.tcsetattr(fd, termios.TCSADRAIN, new)
         while True:
             char = sys.stdin.read(1)
-            print ('输入内容是：',char)
-            if char == 'q': break
+            print("输入内容是：", char)
+            if char == "q":
+                break
     finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old)
+        termios.tcsetattr(fd, termios.TCSADRAIN, old)
+
+
+getch()
