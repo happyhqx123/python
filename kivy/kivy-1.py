6cfff633db81db76a0543c8ee8a2e2c8a597bc3b
happyhqx123 <529503542@qq.com>
2019-08-04 00:57:53 +0800

    kivy-2

--- kivy/kivy-1.py
+++ kivy/kivy-1.py
@@ -1,8 +1,6 @@
 from kivy.app import App
-from kivy.uix.button import Button
-
-class TestApp(App):
+from kivy.uix.label import Label
+class Myapp(App):
     def build(self):
-        return Button(text='hello world')
-
-TestApp().run()
+        return Label(text="老子打死你")
+Myapp().run()
