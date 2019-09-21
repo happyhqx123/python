36030141d6973f81bf878e823a5f4a79c54de84c
happyhqx123 <529503542@qq.com>
2019-09-21 19:59:17 +0800

    tk

---
+++ tk-gui/tk-1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+windows=tk.Tk
+windows.title('第一个tkinter程序')
+windows.geometry('500x300')
+i=tk.Label(windows,text="我要打死你",bg='green',font=("Arial",12),width=30,height=2)
+i.pack()
+windows.mainloop()
+
