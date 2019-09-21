d993f58cabcef81baa525665f00adb214414b2e2
root <529503542@qq.com>
2019-09-21 21:49:47 +0800

    改错

--- tk-gui/tk-1.py
+++ tk-gui/tk-1.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-windows=tk.Tk
+windows=tk.Tk()
 windows.title('第一个tkinter程序')
 windows.geometry('500x300')
 i=tk.Label(windows,text="我要打死你",bg='green',font=("Arial",12),width=30,height=2)
