b6b4e357a1e0a4bcde89bcf28e6e31e8fd806a01
happyhqx123 <529503542@qq.com>
2019-10-05 16:41:41 +0800

    hhh

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-
-from kivy.app import App
-class helloapp(App):
-    pass
-if __name__=='__main__':
-    helloapp.run()
+print("---"*9)
--- sjwl神经网络/sjwl.py
+++ sjwl神经网络/sjwl.py
@@ -1,58 +1,44 @@
-def ycq():  #定义一个预测器
-    a=100
-    b=62.137
-    c=0.5
-    while True:
-        c=c+0.00001
-        #print(c)
-        if c*a >=b:
-            print(c)
-            break
-ycq()
+import numpy as np 
+x= np.array([1,2,3,4,5,6])
+y= np.array([7,8,9,6,5,2])
+z=np.array([[1,3,5],[2,4,6],[7,9,11]])
+f=np.array([2,4,1])
+print("x+y=:",x+y)
+print("x-y=:",x-y)
+print("x*y=:",x*y)
+print("x/y=:",x/y)
+print("x/2=:",x/2.0)
 
 
-def flq():  #定义一个分类器
-    y1=3.0
-    x1=1.0
-
-    y2=1.0
-    x2=3.0
-    a=0.25
-    y=x2*a
-    e=y2-y
-    while True:
-        a=e / x2/4+a 
-        print(a)
-        if a*x2 >y2 :
-            break
-    #print(a)
-    y=x1*a
-    e=2.9-y
-    a=e/x1*0.5+a
-    print(a)
-flq()
-
-
-def s(x):    #定义S函数
-    e=2.71828
-    y=1/((1/e)**x+1)
-    return y
-#print('s函数的返回值是:',s(0.3))
-for i in range(-100, 100):
-    x=i/10
-    print(x,'---',s(x))
+x=np.array([[1,2,3],[7,8,5],[5,8,9],[5,5,9]])
+print('z=:\n',z)
+print('f=:\n',f)
+print("z+f=:\n",z+f)
+print("z-f=:\n",z-f)
+print("z*f=:\n",z*f)
+print("z/f=:\n",z/f)
+print('-'*9,'x=:\n',x,"x转换为一维数组后\n",x.flatten())
+print(x[0])
+print(x[0][1])
+print(x[x>3])
+print('*'*90)
 
 
 
-def yumen(x1,x2):    #定义一个与门
-    #w:权重，x:参数，theta:阀值
-    w1,w2,theta=0.6,0.6,0.7
-    y=w1*x1+w2*x2
-    if y>theta:
-        return  x1,x2,"---",1
-    elif y<=theta:
-        return x1,x2,"---",0
-print(yumen(1,0))
-print(yumen(0,0))
-print(yumen(0,1))
-print(yumen(1,1))
+
+
+
+
+import matplotlib.pyplot as plt
+x= np.arange(0,9,0.1)
+y=np.sin(x)
+y1=np.sin(x)
+y2=np.cos(x)
+plt.plot(x,y1,label='sin')
+plt.plot(x,y2,linestyle='--',label='cos')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('sin & cos')
+plt.legend()
+
+plt.show()
