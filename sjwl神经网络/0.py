2ca0df52be191f72605603282be62b6e79784591
root <529503542@qq.com>
2019-10-10 11:26:46 +0800

    ttt

--- sjwl神经网络/0.py
+++ sjwl神经网络/0.py
@@ -6,6 +6,6 @@ def S(x):    #定义Sigmoid函数
 def R(x):    #定义Relu函数
     return np.maximum(0,x)
 
-x=np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7])
+x=np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,1.0,1.2,1.4])
 print("S--",S(x))
 print("R--",R(x))
