569a2dd4f1c6147070649d98c14fdefc81f33e47
happyhqx123 <529503542@qq.com>
2019-10-16 04:18:11 +0800

    增加mnist.py

--- sjwl神经网络/0.py
+++ sjwl神经网络/0.py
@@ -1,11 +1,33 @@
 import numpy as np
-def S(x):    #定义Sigmoid函数
-    return 1/(1+np.exp(-x))
 
 
-def R(x):    #定义Relu函数
-    return np.maximum(0,x)
+def S(x):  # 定义Sigmoid函数
+    return 1 / (1 + np.exp(-x))
 
-x=np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,1.0,1.2,1.4])
-print("S--",S(x))
-print("R--",R(x))
+
+def R(x):  # 定义Relu函数
+    return np.maximum(0, x)
+
+
+x = np.array([1.0, 0.5])
+w1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+b1 = np.array([0.1, 0.2, 0.3])
+print("x", x.shape)
+print("w1", w1.shape)
+print("b1", b1.shape)
+ai = np.dot(x, w1) + b1
+z1 = S(ai)
+print(ai)
+print(z1)
+w2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+b2 = np.array([0.1, 0.2])
+a2 = np.dot(z1, w2) + b2
+z2 = S(a2)
+print(z2)
+
+
+w3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+b3 = np.array([0.1, 0.2])
+a3 = np.dot(z2, w3) + b3
+z3 = S(a3)
+print(z3)
---
+++ tk-gui/tk-3.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+windows =tk.Tk()
+windows.title("我的第三个tkinter")
+windows.geometry("300x300")
+
